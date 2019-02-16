18456b5ba71950565bbc53f10ee43098436a3c3e
Exalw <exalw@yahoo.de>
2019-02-16 20:39:19 +0100

    Upload Project

---
+++ src/main/java/own/projects/secretSpamer/SecretSpamerApplication.java
@@ -0,0 +1,14 @@
+package own.projects.secretSpamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretSpamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretSpamerApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/own/projects/secretSpamer/SecretSpamerApplicationTests.java
@@ -0,0 +1,17 @@
+package own.projects.secretSpamer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecretSpamerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
